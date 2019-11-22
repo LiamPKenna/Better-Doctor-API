594ac7e5a241810aa85a0b26cc6adf2afb20b23d
LiamPKenna <liampkenna@gmail.com>
2019-11-22 09:17:55 -0800

    add function to handle API requests

--- src/js/api.js
+++ src/js/api.js
@@ -3,8 +3,21 @@ export class ApiRequest {
 
   }
 
-  getDoctorsByIssue(issue) {
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=45.520%2C%20-122.677%2C100&user_location=45.520%2C%20-122.677&skip=0&limit=10&user_key=${process.env.API_KEY}`;
-    console.log(url);
+  getApiResponse(url) {
+    const apiResponse = new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.onload = () => {
+        if (request.status === 200) {
+          const response = JSON.parse(request.response);
+          resolve(response);
+        } else {
+          reject(request.statusText);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    return apiResponse;
   }
 }
