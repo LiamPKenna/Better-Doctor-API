e4e9a7b856289c3c8ed7d4b36b766e27b6e8c21c
LiamPKenna <liampkenna@gmail.com>
2019-11-22 10:16:26 -0800

    add function to hunt for website in doctor practices

--- src/js/doctor.js
+++ src/js/doctor.js
@@ -4,7 +4,18 @@ export class Doctor {
     this.last = doc.profile.last_name;
     this.address = doc.practices[0].visit_address;
     this.phone = doc.practices[0].phones[0].number;
-    this.web = doc.practices[0].website;
+    this.web = this.findWebsite(doc);
     this.newPatients = doc.practices[0].accepts_new_patients;
+  }
+
+  findWebsite(doc) {
+    if (doc.practices[0].website) {
+      return doc.practices[0].website;
+    } else {
+      for (var i = 0; i < doc.practices.length; i++) {
+        if (doc.practices[i].website) return doc.practices[i].website;
+      }
+      return false;
+    }
   }
 }
