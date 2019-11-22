3d48b498ec0acf7b12599962c9d9c044909be3ac
LiamPKenna <liampkenna@gmail.com>
2019-11-22 15:08:28 -0800

    format error responses

--- src/js/doctorSearch.js
+++ src/js/doctorSearch.js
@@ -9,9 +9,16 @@ export class DoctorSearch {
   }
 
   async getDoctorByQuery(query) {
-    const location = (!query.zip) ?
+    let location = (!query.zip) ?
       [45.515,-122.643] :
       await this.apiRequest.getLocation(query.zip);
+    if (!location) {
+      return `
+        <div class="error">
+          <h2>We're sorry! ${query.zip} is not a valid zip code! Please try again!</h2>
+        </div>
+      `;
+    }
     const url = `https://api.betterdoctor.com/2016-03-01/doctors?${query.docName}${query.docFirst}${query.docLast}${query.query}location=${location[0]}%2C%20${location[1]}%2C100&user_location=${location[0]}%2C%20${location[1]}&${query.docGender}sort=distance-asc&skip=0&limit=50&user_key=${process.env.API_KEY}`;
     return this.apiRequest.getApiResponse(url)
       .then((response) => {
@@ -25,7 +32,12 @@ export class DoctorSearch {
           });
           return docDetails.join('');
         } else {
-          return 'No doctors were found that met your criteria. Sorry!';
+          return `
+            <div class="error">
+              <h2>No doctors were found that met your criteria. Sorry!</h2>
+            </div>
+          `;
+
         }
       });
   }
