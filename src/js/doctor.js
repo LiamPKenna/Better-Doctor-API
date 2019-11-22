1d116c566538199ef64a6ccc1158b92d5039ac7f
LiamPKenna <liampkenna@gmail.com>
2019-11-22 10:05:22 -0800

    add Doctor class to store individual doctor details

--- src/js/doctor.js
+++ src/js/doctor.js
@@ -1,16 +1,10 @@
-import { ApiRequest } from './api.js';
-
-export class DoctorSearch {
-  constructor() {
-    this.apiRequest = new ApiRequest();
-  }
-
-  getDoctorByQuery(query) {
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?${query.docName}${query.docFirst}${query.docLast}${query.query}location=45.520%2C%20-122.677%2C100&user_location=45.520%2C%20-122.677&${query.docGender}skip=0&limit=10&user_key=${process.env.API_KEY}`;
-    this.apiRequest.getApiResponse(url)
-      .then((response) => {
-        const doctors = response.data;
-        console.log(doctors);
-      });
+export class Doctor {
+  constructor(doc) {
+    this.first = doc.profile.first_name;
+    this.last = doc.profile.last_name;
+    this.address = doc.practices[0].visit_address;
+    this.phone = doc.practices[0].phones[0].number;
+    this.web = doc.practices[0].website;
+    this.newPatients = doc.practices[0].accepts_new_patients;
   }
 }
